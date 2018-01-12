899c659e196e19c7cf23719ab72e1c44d3057b6a
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-01-11 18:52:26 -0700

    added back previous include

--- RigidBodyKinematicsConfig.cmake
+++ RigidBodyKinematicsConfig.cmake
@@ -9,10 +9,10 @@ endif()
 
 
 if (${IS_FORTUNA})
-	set(RBK_INCLUDE_DIR /home/bebe0705/libs/local/include/RigidBodyKinematics/RigidBodyKinematics.hpp)
+	set(RBK_INCLUDE_DIR /home/bebe0705/libs/local/include/RigidBodyKinematics/)
 	set(RBK_LIBRARY /home/bebe0705/libs/local/lib/libRigidBodyKinematics.so)
 else()
-	set(RBK_INCLUDE_DIR /usr/local/include/RigidBodyKinematics/RigidBodyKinematics.hpp)
+	set(RBK_INCLUDE_DIR /usr/local/include/RigidBodyKinematics/)
 
 	if (APPLE)
 		set(RBK_LIBRARY /usr/local/lib/libRigidBodyKinematics.dylib)
