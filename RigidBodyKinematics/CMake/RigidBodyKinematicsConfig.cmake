7c68ed8967e6f4106e585a7e8511c6e6456f1612
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-04-19 10:52:50 -0600

    updated config file

--- RigidBodyKinematics/CMake/RigidBodyKinematicsConfig.cmake
+++ RigidBodyKinematics/CMake/RigidBodyKinematicsConfig.cmake
@@ -20,29 +20,7 @@
 # SOFTWARE.
 #
 
-if (EXISTS /home/bebe0705/.am_fortuna)
-	set(IS_FORTUNA ON)
-	message("-- This is Fortuna")
-
-else()
-	set(IS_FORTUNA OFF)
-endif()
-
-
-if (${IS_FORTUNA})
-	set(RBK_INCLUDE_DIR /home/bebe0705/libs/local/include/RigidBodyKinematics/)
-	set(RBK_LIBRARY /home/bebe0705/libs/local/lib/libRigidBodyKinematics.so)
-else()
-	set(RBK_INCLUDE_DIR /usr/local/include/RigidBodyKinematics.hpp)
-	if (APPLE)
-		set(RBK_LIBRARY /usr/local/lib/libRigidBodyKinematics.dylib)
-	elseif(UNIX AND NOT APPLE)
-		set(RBK_LIBRARY /usr/local/lib/libRigidBodyKinematics.so)
-	else()
-		message(FATAL_ERROR "Unsupported platform")
-	endif()
-
-endif()
-
+set(RBK_INCLUDE_DIR /home/bebe0705/libs/local/include/RigidBodyKinematics/)
+set(RBK_LIBRARY /home/bebe0705/libs/local/lib/libRigidBodyKinematics.so)
 
 message("-- Found RigidBodyKinematics: " ${RBK_LIBRARY})
