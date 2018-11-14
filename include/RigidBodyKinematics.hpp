d3fdcfe6377c2c03136cc1dd0641186af0b25a63
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2018-11-14 14:44:47 -0700

    updates

--- include/RigidBodyKinematics.hpp
+++ include/RigidBodyKinematics.hpp
@@ -44,7 +44,7 @@ Converts MRP to DCM
 @param sigma MRP vector
 @return dcm DCM matrix
 */
-arma::mat mrp_to_dcm(const arma::vec & sigma);
+	arma::mat::fixed<3,3> mrp_to_dcm(const arma::vec::fixed<3> & mrp);
 
 /**
 Converts DCM to MRP.
@@ -52,7 +52,7 @@ Converts DCM to MRP.
 @param short_rot True if short rotation is desired (default), false otherwise
 @return mrp MRP set
 */
-arma::vec dcm_to_mrp(const arma::mat & dcm, const bool short_rot = true);
+	arma::vec::fixed<3> dcm_to_mrp(const arma::mat::fixed<3,3> & dcm, const bool short_rot = true);
 
 /**
 Converts Quaternions to MRP.
@@ -60,14 +60,14 @@ Converts Quaternions to MRP.
 @param short_rot True if short rotation is desired (default), false otherwise
 @return mrp MRP set
 */
-arma::vec quat_to_mrp(const arma::vec & Q , const bool short_rot = true);
+	arma::vec::fixed<3> quat_to_mrp(const arma::vec::fixed<4> & Q , const bool short_rot = true);
 
 /**
 Converts a set of 321 Euler angles to DCM
 @param euler_angles 321 sequence of Euler angles (rad)
 @return dcm DCM
 */
-arma::mat euler321_to_dcm(const arma::vec & euler_angles);
+	arma::mat::fixed<3,3> euler321_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -75,7 +75,7 @@ Converts a set of (longitude,latitude) angles to DCM
 @param (longitude,latitude) angles (rad)
 @return dcm DCM
 */
-arma::mat longitude_latitude_to_dcm(const arma::vec & euler_angles);
+	arma::mat::fixed<3,3> longitude_latitude_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -83,27 +83,32 @@ Converts a set of 321 Euler angles to mrp
 @param euler_angles 321 sequence of Euler angles (rad)
 @return mrp MRP set
 */
-arma::vec euler321_to_mrp(const arma::vec & euler_angles);
+	arma::vec::fixed<3> euler321_to_mrp(const arma::vec::fixed<3> & euler_angles);
 
 /**
 Computes the time derivative of the attitude set
 assuming torque free rotational dynamics and MRP as attitude coordinate
-@param t Current time
-@param attitude_set mrp + angular velocities
-@param inertia Inertia matrix
+@param[in] t Current time
+@param[in] attitude_set mrp + angular velocities
+@param[in] inertia inertia matrix
+@param[in] L external torque (defaults to (0,0,0))
 @return time derivative of the input attitude set
 */
-arma::vec dXattitudedt(double t, arma::vec attitude_set, arma::mat inertia) ;
+	arma::vec::fixed<6> dXattitudedt(double t, const arma::vec::fixed<6> & attitude_set, const arma::mat::fixed<3,3> & inertia,
+		const arma::vec::fixed<3> & L = arma::zeros<arma::vec>(3)) ;
 
 /**
 Computes the time derivative of the angular velocity set
-assuming torque-free rotation
-@param t Current time
-@param attitude_set mrp + angular velocities
-@param inertia Inertia matrix
+@param[in] t Current time
+@param[in] attitude_set mrp + angular velocities
+@param[in] inertia inertia matrix
+@param[in] L external torque (defaults to (0,0,0))
 @return time derivative of the input angular velocity
 */
-arma::vec domegadt(double t, arma::vec attitude_set, arma::mat & inertia) ;
+	arma::vec::fixed<3> domegadt(double t, 
+		const arma::vec::fixed<6> & attitude_set, 
+		const arma::mat::fixed<3,3> & inertia,
+		const arma::vec::fixed<3> & L = arma::zeros<arma::vec>(3)) ;
 
 /**
 Computes the time derivative of a mrp set given
@@ -112,18 +117,18 @@ a corresponding angular velocity
 @param attitude_set mrp + angular velocities
 @return time derivative of the input mrp
 */
-arma::vec dmrpdt(double t, arma::vec attitude_set );
+	arma::vec::fixed<3> dmrpdt(double t, const arma::vec::fixed<6> & attitude_set );
 
 
 /**
 Returns the shadow set of the input mrp if crossing
 surface is reached
 @param mrp MRP set
-@param force_switch if True, will force the switching of the MRP to its shadow without checking its norm
+@param force_switch if true, will force the switching of the MRP to its shadow without checking its norm
 @return mrp or its shadow set
 
 */
-arma::vec shadow_mrp(const arma::vec & mrp, bool force_switch = false) ;
+	arma::vec::fixed<3> shadow_mrp(const arma::vec::fixed<3> & mrp, bool force_switch = false) ;
 
 
 /**
@@ -131,7 +136,7 @@ Converts MRP to quaternions
 @param sigma MRP vector
 @return quat Unit quaternion
 */
-arma::vec mrp_to_quat(const arma::vec & mrp);
+	arma::vec::fixed<4> mrp_to_quat(const arma::vec::fixed<3> & mrp);
 
 
 /**
@@ -139,14 +144,14 @@ Converts a set of 321 Euler angles to DCM
 @param euler_angles 321 sequence of Euler angles (deg)
 @return dcm DCM
 */
-arma::mat euler321d_to_dcm(arma::vec & euler_angles);
+	arma::mat::fixed<3,3> euler321d_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 /**
 Converts a set of 321 Euler angles to mrp
 @param euler_angles 321 sequence of Euler angles (deg)
 @return mrp MRP set
 */
-arma::vec euler321d_to_mrp(arma::vec & euler_angles);
+	arma::vec::fixed<3> euler321d_to_mrp(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -154,28 +159,28 @@ Converts a set of 313 Euler angles to DCM
 @param euler_angles 313 sequence of Euler angles (deg)
 @return dcm DCM
 */
-arma::mat euler313d_to_dcm(arma::vec & euler_angles);
+	arma::mat::fixed<3,3> euler313d_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 /**
 Converts a set of 313 Euler angles to mrp
 @param euler_angles 313 sequence of Euler angles (deg)
 @return mrp MRP set
 */
-arma::vec euler313d_to_mrp(arma::vec & euler_angles);
+	arma::vec::fixed<3> euler313d_to_mrp(const arma::vec::fixed<3> & euler_angles);
 
 /**
 Converts a set of 321 Euler angles expressed in degrees to DCM
 @param euler_angles 321 sequence of Euler angles (deg)
 @return dcm DCM
 */
-arma::mat euler321d_to_dcm(arma::vec & euler_angles);
+	arma::mat::fixed<3,3> euler321d_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 /**
 Converts a set of 321 Euler angles expressed in degrees to mrp
 @param euler_angles 321 sequence of Euler angles (deg)
 @return mrp MRP set
 */
-arma::vec euler321d_to_mrp(arma::vec & euler_angles);
+	arma::vec::fixed<3> euler321d_to_mrp(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -183,7 +188,7 @@ Converts a set of 313 Euler angles to mrp
 @param euler_angles 313 sequence of Euler angles (rad)
 @return mrp MRP set
 */
-arma::vec euler313_to_mrp(const arma::vec & euler_angles);
+	arma::vec::fixed<3> euler313_to_mrp(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -191,7 +196,7 @@ Converts a set of 313 Euler angles to DCM
 @param euler_angles 313 sequence of Euler angles (rad)
 @return dcm DCM
 */
-arma::mat euler313_to_dcm(const arma::vec & euler_angles);
+	arma::mat::fixed<3,3> euler313_to_dcm(const arma::vec::fixed<3> & euler_angles);
 
 
 /**
@@ -199,98 +204,98 @@ Returns the matrix tilde[x] of the linear operator v|---> cross(x,v)
 @param vec 3-by-1 vector
 @return M Skew-symmetric matrix of the said linear operator
 */
-arma::mat tilde(const arma::vec & vec);
+	arma::mat::fixed<3,3> tilde(const arma::vec::fixed<3> & vec);
 
 /**
 Matrix of the elemental rotation about the first axis of the current frame
 @param angle Rotation angle (rad)
 @return M Elemental rotation matrix
 */
-arma::mat M1(const double angle);
+	arma::mat::fixed<3,3> M1(const double angle);
 
 /**
 Matrix of the elemental rotation about the second axis of the current frame
 @param angle Rotation angle (rad)
 @return M Elemental rotation matrix
 */
-arma::mat M2(const double angle);
+	arma::mat::fixed<3,3> M2(const double angle);
 
 /**
 Matrix of the elemental rotation about the third axis of the current frame
 @param angle Rotation angle (rad)
 @return M Elemental rotation matrix
 */
-arma::mat M3(const double angle);
+	arma::mat::fixed<3,3> M3(const double angle);
 
 /**
 Converts a DCM to the corresponding set of Euler angles
 @param m DCM
 @return angles Sequence of 321 Euler angles angles [yaw,pitch,roll]
 */
-arma::vec dcm_to_euler321(const arma::mat & dcm);
+	arma::vec::fixed<3> dcm_to_euler321(const arma::mat::fixed<3,3> & dcm);
 
 /**
 Converts a DCM to the corresponding set of Euler angles
 @param m DCM
 @return angles Sequence of 313 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec dcm_to_euler313(const arma::mat & dcm);
+	arma::vec::fixed<3> dcm_to_euler313(const arma::mat::fixed<3,3> & dcm);
 
 /**
 Converts a MRP to the corresponding set of Euler angles
 @param sigma MRP vector
 @return angles Sequence of 313 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec mrp_to_euler313(const arma::vec & sigma);
+	arma::vec::fixed<3> mrp_to_euler313(const arma::vec::fixed<3> & mrp);
 
 /**
 Converts a MRP to the corresponding set of Euler angles
 @param sigma MRP vector
 @return angles Sequence of 321 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec mrp_to_euler321(const arma::vec & sigma);
+	arma::vec::fixed<3> mrp_to_euler321(const arma::vec::fixed<3> & mrp);
 
 /**
 Converts a DCM to the corresponding set of Euler angles in degrees
 @param m DCM
 @return angles Sequence of 321 Euler angles angles [yaw,pitch,roll]
 */
-arma::vec dcm_to_euler321d(const arma::mat & dcm);
+	arma::vec::fixed<3> dcm_to_euler321d(const arma::mat::fixed<3,3> & dcm);
 
 /**
 Converts a DCM to the corresponding set of Euler angles in degrees
 @param m DCM
 @return angles Sequence of 313 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec dcm_to_euler313d(const arma::mat & dcm);
+	arma::vec::fixed<3> dcm_to_euler313d(const arma::mat::fixed<3,3> & dcm);
 
 /**
 Converts a MRP to the corresponding set of Euler angles in degrees
 @param sigma MRP vector
 @return angles Sequence of 313 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec mrp_to_euler313d(const arma::vec & sigma);
+	arma::vec::fixed<3> mrp_to_euler313d(const arma::vec::fixed<3> & mrp);
 
 /**
 Converts a MRP to the corresponding set of Euler angles in degrees
 @param sigma MRP vector
 @return angles Sequence of 321 Euler angles angles [right ascension,inclination,longitude]
 */
-arma::vec mrp_to_euler321d(const arma::vec & sigma);
+	arma::vec::fixed<3> mrp_to_euler321d(const arma::vec::fixed<3> & mrp);
 
 /**
 Converts a DCM to a quaternion corresponding to the short-path rotation
 @param dcm DCM
 @return Q Unit quaternion
 */
-arma::vec dcm_to_quat(const arma::mat & dcm) ;
+	arma::vec::fixed<4> dcm_to_quat(const arma::mat::fixed<3,3> & dcm) ;
 
 /**
 Converts a dcm to the principal rotation vector
 @param dcm DCM
 @return prv Principal rotation vector
 */
-arma::vec dcm_to_prv(const arma::mat & dcm) ;
+	arma::vec::fixed<3> dcm_to_prv(const arma::mat::fixed<3,3> & dcm) ;
 
 /**
 Converts a PRV to the corresponding DCM
@@ -298,7 +303,7 @@ Converts a PRV to the corresponding DCM
 @return DCM
 */
 
-arma::mat prv_to_dcm(const arma::vec & prv);
+	arma::mat::fixed<3,3> prv_to_dcm(const arma::vec::fixed<3> & prv);
 
 
 /**
@@ -307,14 +312,23 @@ Converts a PRV to a well-behaved MRP set
 @return MRP set
 */
 
-arma::vec prv_to_mrp(const arma::vec & prv);
+	arma::vec::fixed<3> prv_to_mrp(const arma::vec::fixed<3> & prv);
 
 /**
 Returns the B matrix in the evaluation of the MRP's time derivative (sigma_dot = 1/4 * Bmat(sigma) * omega)
 @param mrp MRP set
 @return instantiated B mtrix
 */
-arma::mat Bmat(const arma::vec & mrp);
+	arma::mat::fixed<3,3> Bmat(const arma::vec::fixed<3> & mrp);
+
+/**
+Returns the partial derivative of mrp_dot with respect to the mrp
+@param attitude_set attitude set comprised of the mrp set and its associated angular velocity
+@return partial derivative of mrp_dot with respect to the mrp  
+*/
+	arma::mat::fixed<3,3> partial_mrp_dot_partial_mrp(const arma::vec::fixed<6> & attitude_set);
+
+
 
 }
 
